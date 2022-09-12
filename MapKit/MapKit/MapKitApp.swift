001a9ca0ec845572444a922234ae067870d1fc4b
Onofras Denis <onofras.denisd@gmail.com>
2022-09-12 11:23:13 +0300

    first commit

---
+++ MapKit/MapKit/MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitApp.swift
+//  MapKit
+//
+//  Created by Denis Onofras on 12.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
